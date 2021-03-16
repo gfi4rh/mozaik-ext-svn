b0a451368450cbecd5b034e636249b8e4461fb26
Regourd Colin <colin.regourd@inetum.world>
2021-03-16 14:56:44 +0100

    comment chalk

--- src/client.js
+++ src/client.js
@@ -6,7 +6,8 @@ const client = mozaik => {
   const apiCalls = {
 
     build( name ){
-      mozaik.logger.info(chalk.yellow(`[jira] calling jenkins.build`));
+
+      //mozaik.logger.info(chalk.yellow(`[jira] calling jenkins.build`));
 
       return fetch(`http://nrh-pic:8080/job/${name.name}/lastBuild/api/json`, {
         method: 'GET',
