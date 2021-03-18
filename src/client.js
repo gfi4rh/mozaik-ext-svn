59afb7a308599dc449c04469ad22dbbfc591e4e0
Regourd Colin <colin.regourd@inetum.world>
2021-03-18 16:11:11 +0100

    init Test widget

--- src/client.js
+++ src/client.js
@@ -3,33 +3,33 @@ import chalk from 'chalk';
 
 const client = mozaik => {
 
-  const apiCalls = {
+	const apiCalls = {
+		
+		build( name ){
 
-    build( name ){
+			mozaik.logger.info(chalk.yellow(`[jenkins] calling jenkins.build`));
 
-      mozaik.logger.info(chalk.yellow(`[jenkins] calling jenkins.build`));
+			return fetch(`http://nrh-pic:8080/job/${name.name}/lastBuild/api/json`, {
+				method: 'GET',
+				headers: {
+					'Accept': 'application/json'
+				}
+			})
+			.then(res => res.json())
+			.then(json => { 
+					if(json.building){
+							mozaik.bus.changeInterval(`jenkins.build.${name.name}`, 5000);
+					} else {
+							const { apisPollInterval } = mozaik.config;
+							mozaik.bus.changeInterval(`jenkins.build.${name.name}`, apisPollInterval);
+					}
+					return json ;
+			});
+		}
 
-      return fetch(`http://nrh-pic:8080/job/${name.name}/lastBuild/api/json`, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json'
-        }
-      })
-      .then(res => res.json())
-      .then(json => { 
-        if(json.building){
-          mozaik.bus.changeInterval(`jenkins.build.${name.name}`, 2000)
-        } else {
-          const { apisPollInterval } = mozaik.config
-          mozaik.bus.changeInterval(`jenkins.build.${name.name}`, apisPollInterval)
-        }
-        return json 
-      })
-    }
+	}
 
-  }
-
-  return apiCalls;
-}
+		return apiCalls;
+};
 
 export default client;
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,10 +2,9 @@ import React, { Component, PropTypes } from 'react'
 import Mozaik                          from 'mozaik/browser';
 import { ListenerMixin }               from 'reflux';
 import reactMixin                      from 'react-mixin';
-import Build                           from './Build.jsx';
 
 
-class Builds extends Component {
+class Test extends Component {
 
     constructor(props) {
         super(props);   
@@ -13,26 +12,26 @@ class Builds extends Component {
 
     render() {
 
-        const { jobs } = this.props;
-
         return (
             <div>
                 <div className="widget__header">
                     <span>
                         <span className="widget__header__subject">{this.props.title}</span>
                     </span>
+                    <span className="widget__header__count">
+                    </span>
                 </div>
                 <div className="widget__body">
-                    {jobs.map(e => <Build name={e.name} title={e.title}/>)}
+                  Hello from test
                 </div>
             </div>
         );
     }
 }
 
-Builds.displayName = 'Builds';
+Test.displayName = 'Test';
 
-reactMixin(Builds.prototype, ListenerMixin);
-reactMixin(Builds.prototype, Mozaik.Mixin.ApiConsumer);
+reactMixin(Test.prototype, ListenerMixin);
+reactMixin(Test.prototype, Mozaik.Mixin.ApiConsumer);
 
-export default Builds;
+export default Test;
