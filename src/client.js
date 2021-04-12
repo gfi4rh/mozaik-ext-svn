44a282df1c67af0c0400518ddc906e062ef6a2eb
Regourd Colin <colin.regourd@inetum.world>
2021-04-12 12:47:04 +0200

    add xml2js

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 import chalk from 'chalk';
+import { spawn } from 'child_process';
+import xml2js from 'xml2js';
 
 const client = mozaik => {
 
@@ -7,7 +9,40 @@ const client = mozaik => {
 
 		lastCommits ( params ) {
 
-			return new Promise(() => {return {alpha : 0}})
+			const commits = spawn(params.svnPath, [
+				'log', 
+				`${params.url}/svn/repo/${params.project}`, 
+				'-l 5',
+				'--xml'
+				], {
+					'--username' : process.env.SVN_USERNAME,
+					'--password' : process.env.SVN_PASSWORD,
+					'--no-auth-cache' : true
+				})
+
+			commits.stdout.on('data', (data) => {
+				var data = data.toString()
+
+				xml2js.parseString(data, (err, result) => {
+					if(err) {
+						throw err;
+					}
+
+					console.log("Result :" +JSON.stringify(result))
+				})
+
+			});
+
+			commits.stderr.on('data', (data) => {
+				console.log("err : " + data);
+			});
+
+			commits.stdin.on('data', (data) => {
+				console.log("in : " +data);
+			});
+
+
+			return Promise.resolve({alpha : 0})
 		}
 		
 	}
