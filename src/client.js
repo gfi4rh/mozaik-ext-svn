65b3aaecbf29d61f3836e1787870355fd9a49e96
Regourd Colin <colin.regourd@inetum.world>
2021-04-12 13:42:34 +0200

    fix client svn

--- src/client.js
+++ src/client.js
@@ -11,48 +11,49 @@ const client = mozaik => {
 
 			mozaik.logger.info(chalk.yellow(`[svn] calling svn.lastCommit`));
 
-			var json = []
+			return new Promise((resolve, reject) => {
 
-			const commits = spawn(params.svnPath, [
-				'log', 
-				`${params.url}/svn/repo/${params.project}`, 
-				'-l 5',
-				'--xml'
-				], {
-					'--username' : process.env.SVN_USERNAME,
-					'--password' : process.env.SVN_PASSWORD,
-					'--no-auth-cache' : true
+				const commits = spawn(params.svnPath, [
+					'log', 
+					`${params.url}/svn/repo/${params.project}`, 
+					'-l 5',
+					'--xml'
+					], {
+						'--username' : process.env.SVN_USERNAME,
+						'--password' : process.env.SVN_PASSWORD,
+						'--no-auth-cache' : true
 				})
-
-			commits.stdout.on('data', (data) => {
-				var input = data.toString()
-
-				xml2js.parseString(input, (err, result) => {
-					if(err) {
-						throw err;
-					}
-					json = result.log.logentry.map((e) => {
-						return {
-							id : e.$.revision,
-							author : e.author[0],
-							date : e.date[0],
-							msg : e.msg[0].replace(/\r\n/g, ' ')
+	
+				commits.stderr.on('data', (data) => {
+					console.log("err : " + data);
+				});
+	
+				commits.stdin.on('data', (data) => {
+					console.log("in : " +data);
+				});
+	
+				commits.stdout.on('data', (data) => {
+					var input = data.toString()
+	
+					xml2js.parseString(input, (err, result) => {
+						if(err) {
+							throw err;
 						}
+						let json = result.log.logentry.map((e) => {
+							return {
+								id : e.$.revision,
+								author : e.author[0],
+								date : e.date[0],
+								msg : e.msg[0].replace(/\r\n/g, ' ')
+							}
+						})
+						resolve(json)
 					})
+	
 				})
 
-			});
+			}).then(res => res)
 
-			commits.stderr.on('data', (data) => {
-				console.log("err : " + data);
-			});
-
-			commits.stdin.on('data', (data) => {
-				console.log("in : " +data);
-			});
-
-
-			return Promise.resolve(json)
 		}
 		
 	}
