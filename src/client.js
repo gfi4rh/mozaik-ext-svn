a87b91e39405c6b9da87eff2b1722498e7d3b825
Regourd Colin <colin.regourd@inetum.world>
2021-05-28 13:32:58 +0200

    test svn connection before call

--- src/client.js
+++ src/client.js
@@ -5,24 +5,36 @@ import xml2js from 'xml2js';
 
 const client = mozaik => {
 
-	const apiCalls = {
+	return {
 
 		lastCommits ( params ) {
 
 			mozaik.logger.info(chalk.yellow(`[svn] calling svn.lastCommit`));
 
+
 			return new Promise((resolve, reject) => {
 
-				const commits = spawn(params.svnPath, [
-					'log', 
-					`${params.url}/svn/repo/${params.project}`, 
-					'-l 5',
-					'--xml'
-					], {
-						'--username' : process.env.SVN_USERNAME,
-						'--password' : process.env.SVN_PASSWORD,
-						'--no-auth-cache' : true
+				let commits = null
+
+				fetch(params.svnPath, {
+					method : 'GET'
 				})
+				.then(res => {
+					if(res.status === 200){
+						commits = spawn(params.svnPath, [
+							'log', 
+							`${params.url}/svn/repo/${params.project}`, 
+							'-l 5',
+							'--xml'
+							], {
+								'--username' : process.env.SVN_USERNAME,
+								'--password' : process.env.SVN_PASSWORD,
+								'--no-auth-cache' : true
+						})
+					}
+				})
+
+
 	
 				commits.stderr.on('data', (data) => {
 					console.log("err : " + data);
@@ -64,8 +76,6 @@ const client = mozaik => {
 		}
 		
 	}
-
-	return apiCalls;
 
 };
 
